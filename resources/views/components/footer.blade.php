19771c14d027f78e1eca724b52d63a0dd339a04b
A1ves <ayronwork@gmail.com>
2025-12-25 16:26:38 -0300

    fix: Ajustando models e migrates

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-white border-t-2 p-4">
+  <p class="text-center">
+    Criado por Ayron Alves. Link do código <a href="#" class="underline">aqui</a>
+  </p>
+</footer>
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name') }}</title>
+    @vite('resources/css/app.css')
+  </head>
+
+  <body class="bg-[#FFEDD6]">
+  {{-- HEADER --}}
+    <x-header />
+
+  {{-- CONTEUDO --}}
+    {{ $slot }}
+
+  {{-- FOOTER --}}
+  <x-footer />
+  </body>
+</html>
